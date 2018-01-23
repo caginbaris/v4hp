ba347ebff6e1cd110fdb178d3b7d396e58deb637
caginbaris <31400655+caginbaris@users.noreply.github.com>
2018-01-23 08:27:44 +0200

    carry on config

---
+++ Inc/board_specific_definitions.h
@@ -0,0 +1,13 @@
+#ifndef __board_specific_definitions_h
+#define __board_specific_definitions_h
+
+#define CTB_TCR 0
+#define CTB_HF2 1
+#define CTB_HF3 0
+#define CTB_HF4 0
+
+
+/*conditional Parameters*/
+
+
+#endif
